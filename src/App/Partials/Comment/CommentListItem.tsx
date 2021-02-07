3e107d96f66eb61040a91a53677691e894e3bcd1
Daniel Pfisterer <info@daniel-pfisterer.de>
2021-02-07 09:30:16 +0100

    Prepare for comment replies

--- src/App/Partials/Comment/CommentListItem.tsx
+++ src/App/Partials/Comment/CommentListItem.tsx
@@ -1,46 +1,75 @@
 import { stringify } from 'querystring';
-import React, { FunctionComponent, useCallback, useEffect, useState } from 'react';
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import { useSelector } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import TimeAgo from 'react-timeago';
 import { usePrevious } from 'react-use';
-import { Card, CardBody, CardSubtitle, Col, Row } from 'reactstrap';
+import { Badge, Card, CardBody, CardSubtitle, Col, Row } from 'reactstrap';
 
-import { Comment, Debate, GlobalState, PrivateRequestKeys, PublicRequestKeys, User } from '../../../Library';
+import {
+  Comment,
+  Debate,
+  GlobalState,
+  PrivateRequestKeys,
+  PublicRequestKeys,
+  User,
+} from '../../../Library';
 import { CommentForm } from './CommentForm';
 import { DismissButton } from './DismissButton';
 
 export interface CommentListItemProps extends Comment {
   canWrite: boolean;
   debateId: Debate['id'];
-  onSubmit?: () => void; 
+  onSubmit?: () => void;
 }
 
-export const CommentListItem: FunctionComponent<CommentListItemProps> = (props) => {
-  const {
-    onSubmit,
-  } = props;
+export const CommentListItem: FunctionComponent<CommentListItemProps> = (
+  props,
+) => {
+  const { onSubmit } = props;
 
   const author = props.user as User;
 
   const user = useSelector<GlobalState, GlobalState[PrivateRequestKeys.User]>(
-    state => state[PrivateRequestKeys.User]
+    (state) => state[PrivateRequestKeys.User],
   );
 
-  const {
-    location,
-  } = useSelector<GlobalState, GlobalState[PublicRequestKeys.Router]>(
-    state => state[PublicRequestKeys.Router]
-  );
+  const { location } = useSelector<
+    GlobalState,
+    GlobalState[PublicRequestKeys.Router]
+  >((state) => state[PublicRequestKeys.Router]);
 
   const prevLocation = usePrevious(location);
 
-  const searchQuery = '?' + stringify({
-    edit: 'comment',
-    id: props.id
-  });
+  const editQuery =
+    '?' +
+    stringify({
+      edit: 'comment',
+      id: props.id,
+    });
 
-  const [canWrite, setCanWrite] = useState(props.canWrite && user.result?.id === author?.id);
+  const replyQuery =
+    '?' +
+    stringify({
+      edit: 'reply',
+      id: props.id,
+    });
+
+  const viewReplyQuery =
+    '?' +
+    stringify({
+      view: 'replies',
+      id: props.id,
+    });
+
+  const [canWrite, setCanWrite] = useState(
+    props.canWrite && user.result?.id === author?.id,
+  );
   const [shouldRedirect, setShouldRedirect] = useState(false);
 
   const createdAt = new Date(props.created_at).toUTCString();
@@ -55,7 +84,6 @@ export const CommentListItem: FunctionComponent<CommentListItemProps> = (props) 
     }
   }, [onSubmit, shouldRedirect]);
 
-
   useEffect(() => {
     if (canWrite !== (props.canWrite && user.result?.id === author?.id)) {
       setCanWrite(!canWrite);
@@ -64,32 +92,57 @@ export const CommentListItem: FunctionComponent<CommentListItemProps> = (props) 
     if (shouldRedirect) {
       setShouldRedirect(false);
     }
-  }, [author?.id, canWrite, location.search, prevLocation?.search, props.canWrite, searchQuery, shouldRedirect, user.result?.id]);
-  
+  }, [
+    author?.id,
+    canWrite,
+    location.search,
+    prevLocation?.search,
+    props.canWrite,
+    editQuery,
+    shouldRedirect,
+    user.result?.id,
+  ]);
+
   return (
     <div className="comment-list-item">
       <Row>
         <Col>
           <Card>
             <CardBody>
-            <CardSubtitle>
-              <small>
-                {author && (
-                  <Link to={`/user/${user.result?.id === author?.id ? 'me' : author.id}`}>
-                    {user.result?.id === author?.id ? 'You' : (
-                      <>{author.username}</>
-                    )}
-                  </Link>
-                )} <span>
-                  commented <i><TimeAgo date={props.created_at} /></i> {createdAt !== updatedAt && (
-                  <>
-                    and edited this debate <i><TimeAgo date={props.updated_at} /></i>.
-                  </>
-                )} </span>
-              </small>
+              <CardSubtitle>
+                <small>
+                  {author && (
+                    <Link
+                      to={`/user/${
+                        user.result?.id === author?.id ? 'me' : author.id
+                      }`}
+                    >
+                      {user.result?.id === author?.id ? (
+                        'You'
+                      ) : (
+                        <>{author.username}</>
+                      )}
+                    </Link>
+                  )}{' '}
+                  <span>
+                    commented{' '}
+                    <i>
+                      <TimeAgo date={props.created_at} />
+                    </i>{' '}
+                    {createdAt !== updatedAt && (
+                      <>
+                        and edited this debate{' '}
+                        <i>
+                          <TimeAgo date={props.updated_at} />
+                        </i>
+                        .
+                      </>
+                    )}{' '}
+                  </span>
+                </small>
               </CardSubtitle>
               <div className={'card-text'}>
-                {location.search !== searchQuery ? (
+                {location.search !== editQuery ? (
                   <p>{props.comment}</p>
                 ) : (
                   <>
@@ -98,10 +151,10 @@ export const CommentListItem: FunctionComponent<CommentListItemProps> = (props) 
                       pathname={location.pathname}
                       title="Cancel"
                     />
-                    <CommentForm 
+                    <CommentForm
                       commentId={props.id}
                       debateId={props.debateId}
-                      defaultData={{comment: props.comment}}
+                      defaultData={{ comment: props.comment }}
                       dismissElement={
                         <DismissButton
                           className="btn-sm mr-3"
@@ -117,19 +170,44 @@ export const CommentListItem: FunctionComponent<CommentListItemProps> = (props) 
                 )}
               </div>
               <div className="comment-list-item-settings">
-                {canWrite && location.search !== searchQuery && (
-                  <Link to={location.pathname + searchQuery} className="p-0 btn btn-none btn-sm">
-                    <i className="fa fa-edit" /> Edit
-                  </Link>
-                )}
+                <Row>
+                  <Col>
+                    <Link
+                      to={location.pathname + viewReplyQuery}
+                      className="p-0 mr-3 btn btn-none btn-sm"
+                      title="Show relies"
+                    >
+                      <Badge>
+                        {(props.replies && props.replies.length) || 0}
+                      </Badge>{' '}
+                      Replies <i className="fa fa-chevron-right" />
+                    </Link>
+                  </Col>
+                  <Col className="text-right">
+                    {canWrite && location.search !== editQuery && (
+                      <>
+                        <Link
+                          to={location.pathname + replyQuery}
+                          className="p-0 mr-3 btn btn-none btn-sm"
+                        >
+                          <i className="fa fa-reply" /> Reply
+                        </Link>
+                        <Link
+                          to={location.pathname + editQuery}
+                          className="p-0 btn btn-none btn-sm"
+                        >
+                          <i className="fa fa-edit" /> Edit
+                        </Link>
+                      </>
+                    )}
+                  </Col>
+                </Row>
               </div>
             </CardBody>
           </Card>
         </Col>
       </Row>
-      {shouldRedirect && (
-        <Redirect to={location.pathname} />
-      )}
+      {shouldRedirect && <Redirect to={location.pathname} />}
     </div>
   );
 };
--- src/Library/Scroll/SetScrollPosition.tsx
+++ src/Library/Scroll/SetScrollPosition.tsx
@@ -4,13 +4,13 @@ import { connect } from 'react-redux';
 import { GlobalState, PublicRequestKeys } from '../Redux';
 
 interface ScrollPositionProps {
-  [PublicRequestKeys.Router]: GlobalState[PublicRequestKeys.Router],
+  [PublicRequestKeys.Router]: GlobalState[PublicRequestKeys.Router];
   name?: string;
   children: ReactNode;
 }
 
-interface Coords { 
-  x: number; 
+interface Coords {
+  x: number;
   y: number;
 }
 
@@ -22,11 +22,13 @@ export class SetScrollPositionBase extends React.Component<ScrollPositionProps> 
     this.body = document.body as HTMLBodyElement;
   }
 
-  private get cookieName (): string {
-    return this.props.name ? `scrollPosition_${this.props.name}` : 'scrollPosition';
+  private get cookieName(): string {
+    return this.props.name
+      ? `scrollPosition_${this.props.name}`
+      : 'scrollPosition';
   }
 
-  private setPosition () {
+  private setPosition() {
     const position = {
       x: window.scrollX,
       y: window.scrollY,
@@ -40,42 +42,41 @@ export class SetScrollPositionBase extends React.Component<ScrollPositionProps> 
     Cookies.set(this.cookieName, JSON.stringify(position), {
       expires: 1,
     });
-  }
+  };
 
-  public componentDidMount () {
+  public componentDidMount() {
     this.body.onunload = this.handleUnload;
 
     const positionFromCookie: string | undefined = Cookies.get(this.cookieName);
-    const position: Coords | undefined = positionFromCookie && JSON.parse(positionFromCookie);
-    
+    const position: Coords | undefined =
+      positionFromCookie && JSON.parse(positionFromCookie);
+
     if (position) {
       Cookies.remove(this.cookieName);
 
       setTimeout(() => {
-        window.scrollTo(position.x, position.y)
-      }, 1000)
+        window.scrollTo(position.x, position.y);
+      }, 1000);
     }
   }
 
-  public componentDidUpdate (prevProps: ScrollPositionProps) {
-    const { 
-      location
-    } = this.props.router;
+  public componentDidUpdate(prevProps: ScrollPositionProps) {
+    const { location } = this.props.router;
 
-    const pattern = /\?.*(modal=|edit=)/;
+    const pattern = /\?.*(modal=|edit=|view=)/;
     const isModal = pattern.test(location.search);
     const isModalPrev = pattern.test(prevProps.router.location.search);
-    
+
     if (!isModal && !isModalPrev && location !== prevProps.router.location) {
       window.scrollTo(0, 0);
     }
   }
 
-  public componentWillUnmount () {
+  public componentWillUnmount() {
     this.body.onunload = null;
   }
 
-  public render () {
+  public render() {
     return this.props.children;
   }
 }
